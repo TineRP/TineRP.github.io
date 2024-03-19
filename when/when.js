c83a0264ea354b0fb72135fcd27a69797776a84c
au637832 <201907152@post.au.dk>
2024-03-19 09:41:06 +0100

    added image and fixed dropdown

--- when/when.js
+++ when/when.js
@@ -1,9 +1,17 @@
-const options = document.querySelectorAll('#month-dropdown');
+/*const options = document.querySelectorAll('#month-dropdown');
 
 options.forEach(option => {
     option.addEventListener('click', function() {
         window.location.href = '../farve/farve.html'
     });
+});*/
+
+
+const monthDropdown = document.getElementById('month-dropdown');
+
+monthDropdown.addEventListener('change', function() {
+    const selectedMonth = this.value;
+    window.location.href = '../farve/farve.html';
 });
 
 const storedData = localStorage.getItem('information')
