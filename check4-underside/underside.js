d22402d3174d4cb56fa54b451217dd583fd99359
au637832 <201907152@post.au.dk>
2024-04-25 14:35:38 +0200

    fixed gills

--- check4-underside/underside.js
+++ check4-underside/underside.js
@@ -4,37 +4,6 @@ const container = document.getElementById('buttons-container')
 const userSelections = JSON.parse(localStorage.getItem('answers'))
 
 const modelPrediction = JSON.parse(localStorage.getItem('modelPrediction'))
-
-// Get the image element
-var image = document.querySelector('.gills');
-
-// Create a canvas element
-var canvas = document.createElement('canvas');
-var context = canvas.getContext('2d');
-
-// Set canvas dimensions to match the image
-canvas.width = image.clientWidth; // Use clientWidth to get the displayed width
-canvas.height = image.clientHeight; // Use clientHeight to get the displayed height
-
-// Draw the image onto the canvas
-context.drawImage(image, 0, 0, canvas.width, canvas.height);
-
-// Add text to the canvas
-var text = 'Gills';
-var fontSize = 50;
-context.font = fontSize + 'px Roboto, sans-serif'; // Set font family to Roboto
-context.fillStyle = 'black';
-
-// Calculate text position to center horizontally and vertically
-var textWidth = context.measureText(text).width;
-var x = (canvas.width - textWidth) / 2;
-var y = (canvas.height + fontSize) / 2; // Adjust for baseline
-
-context.fillText(text, x, y); // Draw text
-
-// Replace the image with the canvas
-image.parentNode.replaceChild(canvas, image);
-
 const button = document.createElement("button");
 button.textContent = "Not sure";
 container.append(button);
