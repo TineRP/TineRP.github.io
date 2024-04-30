74d095731e87b83ebacccb460ae38d1c4606ce0c
au637832 <201907152@post.au.dk>
2024-04-30 09:36:17 +0200

    updated burger on front page

--- index.js
+++ index.js
@@ -329,8 +329,36 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+const submenuItems = document.querySelectorAll('.submenu a');
+
+
+submenuItems.forEach(submenuItem => {
+  submenuItem.addEventListener('click', function() {
+    localStorage.setItem("clickedMushroom", JSON.stringify(submenuItem.textContent));
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const burgerIcon = document.querySelector('.burger-icon');
+  const menuItems = document.querySelector('.menu-items');
+  const speciesMenuItem = document.getElementById('species-menu');
+  const submenu = document.querySelector('.submenu');
+  submenu.style.display = 'none';
+  menuItems.style.display = 'none';
+ 
+
+  burgerIcon.addEventListener('click', function () {  
+    menuItems.style.display = menuItems.style.display === 'none' ? 'block' : 'none';
+  });
+
+  speciesMenuItem.addEventListener('click', function (event) {
+  const submenu = this.nextElementSibling;
+  submenu.style.display = submenu.style.display === 'none' ? 'block' : 'none'; 
+  });
+});
 
 
 mobilenetDemo();
+
 
 
